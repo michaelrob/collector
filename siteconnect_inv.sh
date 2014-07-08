d0a05301fb4cc4f4dec4842cc5cb3c1582952801
Michael Robinson <seraph890@gmail.com>
2014-07-08 13:20:56 +1000

    added siteconnect_in

---
+++ siteconnect_inv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This script takes a siteconnect requester ID and shows the last update request and response for this channel
+
+path="cat /usr/local/siteminder/var/log/siteconnect-comms.log"
+oldpath="bzcat /usr/local/siteminder/var/log/siteconnect-comms.log.****-**-**.bz2"
+
+updateCount=$($path | grep "siteconnect-$1-inventory" | grep "RESPONSE" | cut -c '01-23')
+
+if [ -z "$updateCount" ]
+then
+  echo -e "\nThere have been no updates recently\n"
+else
+  #lastupdate=$(echo $updateCount | cut -c '1-23')
+  $path | grep "$lastupdate"
+fi
+
+oldUpdateCount=$($oldpath | grep "siteconnect-$1-inventory" | grep "RESPONSE" | cut -c '01-23')
+
+if [ -z "$oldUpdateCount" ]
+then
+  echo -e "\nThere have been no updates in the last few weeks\n"
+else
+  oldlastupdate=$(echo $oldUpdateCount | rev | cut -c '-23' | rev )
+  oldtoken=$($oldpath | grep "$oldlastupdate" | cut -c '64-103')
+  $oldpath | grep "$oldtoken" | grep "oldlastupdate"
+fi
