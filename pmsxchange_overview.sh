3f616b2c42079144e3b8262d4ebbc816a2de2ff0
Michael Robinson <seraph890@gmail.com>
2014-07-10 15:18:26 +1000

    basic changes to pmsx_overview. updated siteconnect_overview to take arguments

--- pmsxchange_overview.sh
+++ pmsxchange_overview.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # No input needed, just run this script as is and it will spit out all of the requster IDs that have been sent to us
 
-echo -e "Collecting partners...\n"
+echo "Collecting partners...\n"
 
 today=$(date '+20%y-%m-%d')
 
-results=$(egrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/pmsxchangev2-access.* | sort -r -t: -k2 | uniq -f 1 | sed 's/pmsxchangev2-access.//' | sed -e "s/log:Req/$today,Req/" | sed 's/:Req/,Req/' |   sed 's/\/usr\/local\/siteminder\/var\/    log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//')
+echo "Collecting recent PMS partners...\n"
 
-oldResults=$(bzegrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/pmsxchangev2-access.log.****-**-**.bz2 | sort -r -t: -k2 | uniq -f 1 | sed 's/pmsxchangev2-access.//' | sed     's/log:Req/     Today,Req/   ' | sed 's/:Req/,Req/' |   sed 's/\/   usr\/local\/siteminder\/var\/log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//' | sed 's/.bz2//')
+recent=$(egrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/pmsxchangev2-access.* | sort -r -t: -k2 | uniq -f 1 | sed 's/pmsxchangev2-access.//' | sed -e "s/log:Req/$today,Req/" | sed 's/:Req/,Req/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//')
 
-output=$(echo "$results" && echo "$oldResults")
+echo "${recent}" | sort -t\, -k2 | uniq -s 11
 
-echo -e "${output}" | sort -r -t\, -k2 | uniq -s 11
+echo "\n"
 
-# sends output to CSV
-csv=$(echo "${output}" | sort -r -t\, -k2 | uniq -s 11)
-echo "$csv" | sort -t\, -k2 > pmsxchange.csv
+echo "Collecting aged PMS partners...\n"
+
+aged=$(bzegrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/pmsxchangev2-access.log.****-**-**.bz2 | sort -r -t: -k2 | uniq -f 1 | sed 's/pmsxchangev2-access.//' | sed 's/log:Req/Today,Req/' | sed 's/:Req/,Req/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//' | sed 's/.bz2//')
+
+echo "${aged}" | sort -t\, -k2 | uniq -s 11
--- siteconnect_overview.sh
+++ siteconnect_overview.sh
@@ -5,34 +5,45 @@ echo "Collecting partners from recent logs...\n"
 
 today=$(date '+20%y-%m-%d')
 
-echo "Collecting partners who have requested inventory..."
+while getopts ":ra" opt; do
+  case $opt in
+    r)
+      echo "Collecting partners who have requested inventory..."
 
-comms=$(egrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.* | sort -t: -k2 | uniq | sed "s/siteconnect-comms.log:/$today,/" | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
+      comms=$(egrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.* | sort -t: -k2 | uniq | sed "s/siteconnect-comms.log:/$today,/" | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
-echo "Collecting partners who have requested reservations...\n"
+      echo "Collecting partners who have requested reservations...\n"
 
-access=$(egrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/siteconnect-access.* | sort -r -t: -k2 | uniq -f 1 | sed 's/siteconnect-access.//' | sed "s/log:Req/$today,Req/" | sed 's/:Req/,Req/' |   sed 's/\/usr\/local\/siteminder\/var\/log\///   ' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//')
+      access=$(egrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/siteconnect-access.* | sort -r -t: -k2 | uniq -f 1 | sed 's/siteconnect-access.//' | sed "s/log:Req/$today,Req/" | sed 's/:Req/,Req/' |   sed 's/\/usr\/local\/siteminder\/var\/log\///   ' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//')
 
-echo "Reservations\n=================================\n"
-echo "${access}" | sort -t\, -k2 | uniq -s 11
-echo "\n"
+      echo "Reservations\n=================================\n"
+      echo "${access}" | sort -t\, -k2 | uniq -s 11
+      echo "\n"
 
-echo "Inventory\n=================================\n"
-echo "${comms}" | sort -t\, -k2 | uniq -s 11
+      echo "Inventory\n=================================\n"
+      echo "${comms}" | sort -t\, -k2 | uniq -s 11
 
-echo "\n"
+      echo "\n"
+    ;;
+    a)
+      echo "Collecting partners from aged logs who have requested inventory..."
 
-echo "Collecting partners from aged logs who have requested inventory..."
+      oldComms=$(bzegrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.log.****-**-**.bz2 | sort -t: -k2 | uniq | sed 's/siteconnect-comms.log:/today,/' | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/.bz2//' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
-oldComms=$(bzegrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.log.****-**-**.bz2 | sort -t: -k2 | uniq | sed 's/siteconnect-comms.log:/today,/' | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/.bz2//' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
+      echo "Collecting partners from aged logs who have requested reservations...\n"
 
-echo "Collecting partners from aged logs who have requested reservations...\n"
+      oldAccess=$(bzegrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/siteconnect-access.log.****-**-**.bz2 | sort -r -t: -k2 | uniq -f 1 | sed 's/siteconnect-access.//' | sed 's/log:Req/     Today,Req/' | sed 's/:Req/,Req/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//' | sed 's/.bz2//')
 
-oldAccess=$(bzegrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/siteconnect-access.log.****-**-**.bz2 | sort -r -t: -k2 | uniq -f 1 | sed 's/siteconnect-access.//' | sed 's/log:Req/     Today,Req/' | sed 's/:Req/,Req/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//' | sed 's/.bz2//')
+      echo "Reservations\n=================================\n"
+      echo "${oldAccess}" | sort -t\, -k2 | uniq -s 11
+      echo "\n"
 
-echo "Reservations\n=================================\n"
-echo "${oldAccess}" | sort -t\, -k2 | uniq -s 11
-echo "\n"
-
-echo "Inventory\n=================================\n"
-echo "${oldComms}" | sort -t\, -k2 | uniq -s 11
+      echo "Inventory\n=================================\n"
+      echo "${oldComms}" | sort -t\, -k2 | uniq -s 11
+    ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+    ;;
+  esac
+done
