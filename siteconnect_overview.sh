f6ff9cbb665b947865394c8530ae6464486e0945
Michael Robinson <seraph890@gmail.com>
2014-07-10 10:17:28 +1000

    updated log output for siteconnect overview

--- siteconnect_overview.sh
+++ siteconnect_overview.sh
@@ -5,7 +5,7 @@ echo "Collecting partners from recent logs...\n"
 
 today=$(date '+20%y-%m-%d')
 
-echo "Collecting partners who have sent us inventory...\n"
+echo "Collecting partners who have requested inventory...\n"
 
 comms=$(egrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.* | sort -t: -k2 | uniq | sed "s/siteconnect-comms.log:/$today,/" | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
@@ -22,7 +22,7 @@ echo "${comms}" | sort -t\, -k2 | uniq -s 11
 
 echo "\n"
 
-echo "Collecting partners from aged logs who have sent us inventory...\n"
+echo "Collecting partners from aged logs who have requested inventory...\n"
 
 oldComms=$(bzegrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.log.****-**-**.bz2 | sort -t: -k2 | uniq | sed 's/siteconnect-comms.log:/today,/' | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/.bz2//' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
