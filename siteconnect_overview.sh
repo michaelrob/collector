ce1d13e16d43c0ca38e14dcb938056169ae3546c
Michael Robinson <seraph890@gmail.com>
2014-07-10 13:29:24 +1000

    updates to siteconnect overview

--- siteconnect_overview.sh
+++ siteconnect_overview.sh
@@ -5,11 +5,11 @@ echo "Collecting partners from recent logs...\n"
 
 today=$(date '+20%y-%m-%d')
 
-echo "Collecting partners who have requested inventory...\n"
+echo "Collecting partners who have requested inventory..."
 
 comms=$(egrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.* | sort -t: -k2 | uniq | sed "s/siteconnect-comms.log:/$today,/" | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
-echo "Collecting partners who have requested reservations..."
+echo "Collecting partners who have requested reservations...\n"
 
 access=$(egrep -Ho 'RequestorID ID="(.{1,20})" Type=' /usr/local/siteminder/var/log/siteconnect-access.* | sort -r -t: -k2 | uniq -f 1 | sed 's/siteconnect-access.//' | sed "s/log:Req/$today,Req/" | sed 's/:Req/,Req/' |   sed 's/\/usr\/local\/siteminder\/var\/log\///   ' | sed 's/log.//' | sed 's/" Type=//' | sed 's/RequestorID ID="//')
 
@@ -22,7 +22,7 @@ echo "${comms}" | sort -t\, -k2 | uniq -s 11
 
 echo "\n"
 
-echo "Collecting partners from aged logs who have requested inventory...\n"
+echo "Collecting partners from aged logs who have requested inventory..."
 
 oldComms=$(bzegrep -Ho 'siteconnect-(.{1,3}).*RESPONSE' /usr/local/siteminder/var/log/siteconnect-comms.log.****-**-**.bz2 | sort -t: -k2 | uniq | sed 's/siteconnect-comms.log:/today,/' | sed 's/siteconnect-//' | sed 's/comms.log.//' | sed 's/:siteconnect-/,/' | sed 's/.bz2//' | sed 's/\/usr\/local\/siteminder\/var\/log\///' | sed -e 's/-inventoryJob-[^>]*- \[RESPONSE//')
 
